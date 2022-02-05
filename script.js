279751194a542b1ce33fe590ecb0a97610085f20
anmolbansal7 <abanmolbansal5@gmail.com>
2022-02-06 01:09:59 +0530

    more updates

--- script.js
+++ script.js
@@ -14,37 +14,67 @@ function openDiscord() {
 function searchText() {
 	document.querySelector("#search").disabled = true;
 	document.querySelector("#search").innerHTML = "Searching in the pile...";
+	var rejected = document.querySelector("#rejected");
+	var selected = document.querySelector("#selected");
+	var form = document.querySelector("#mainForm");
 	output = "";
 	var search = document.querySelector(".input").value.toUpperCase().trim();
 	const adg = `https://recruitment2022.herokuapp.com/user/getResult?regno=${search}`;
 	fetch(adg)
 		.then(function (res) {
+			// console.log(res.json());
 			return res.json();
 		})
 		.then(function (data) {
+			console.log(data);
+			//No Record Found
 			if (data.message == "User does Not Exist") {
 				if (search == "") {
-					output += `<p>Please enter a Registration Number</p>`;
+					// output += `<p class="error">Please enter a Registration Number</p>`;
+					output += `<p class="error">${data.status}</p>`;
 				} else {
-					output += `<p>Uh oh! Please enter the correct Registration Number.</p>`;
+					output += `<p class="error">Uh oh! Please enter the correct Registration Number.</p>`;
 				}
-			} else if (
+			}
+			//Rejected
+			else if (
 				data.message ==
 				"Hey, you missed it by a narrow margin. All the best for your future endeavours!"
 			) {
-				output += `<p>${data.message}</p>`;
+				// output += `<p>${data.message}</p>`;
+				rejected.style.display = "flex";
+				mainForm.style.display = "none";
 			} else if (
 				data.message ==
 				"Hey, you missed it by a narrow margin. See you next year. Till then keep hustling."
 			) {
-				output += `<p>${data.message}</p>`;
-			} else {
-				output += `<div class="container info">      
+				//output += `<p>${data.message}</p>`;
+				rejected.style.display = "flex";
+				mainForm.style.display = "none";
+			}
+			//Selected in Tech
+			else {
+				/*output += `<div class="container info">   
+				
             <p>
                 ${data.message}
             </p> 
-            </div>`;
+            </div >`;*/
+				selected.style.display = "flex";
+				mainForm.style.display = "none";
 			}
+			//Selected in Management
+
+			//Selected in Design
+
+			//Selected in TM
+
+			//Selected in TD
+
+			//Selected in MD
+
+			//Selected in TMD
+
 			document.querySelector(".output").innerHTML = output;
 			document.querySelector("#search").disabled = false;
 			document.querySelector("#search").innerHTML =
